0663e245e44e8c827f8d32ea2777e72a587ef0a6
jeremylinlin <linlin_19930805@yahoo.com>
2014-08-19 14:14:43 +0800

    Created a User Model

---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+	# attr_accessible :name, :email #define the attrs that can be accessed from the outside
+		# the code commented above is no longer available in Rails 4, check out http://t.cn/RP8jkg1 for reference
+end
