999d41394a39b539a14f2919ceeb8fed7055751b
jeremylinlin <linlin_19930805@yahoo.com>
2014-08-24 11:55:21 +0800

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @page_title = "Sign Up"
+    @user = User.new
   end
 
   def show
@@ -8,4 +9,20 @@ class UsersController < ApplicationController
   	@page_title = User.find(params[:id]).name
   end
 
+  def create
+  	@page_title = "CREATE"
+	@user = User.new(user_params)
+	if @user.save
+		flash[:success] = "Welcome to Ruby on Rails Tutorial!"
+		redirect_to @user
+	else
+		render 'new'
+	end
+  end
+
+	private
+		def user_params
+			params.require(:user).permit(:name, :password, :password_confirmation, :email)
+		end	
+  
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,13 +4,6 @@ describe "UserPages" do
 	
 	subject	{ page }
 
-	describe "signup page" do
-		before	{ visit signup_path }
-
-		it	{ should have_title("Sign Up") }
-		it	{ should have_content("Sign Up") } 
-	end
-
 	describe "profile page" do
 		let(:user)	{ FactoryGirl.create(:user) }
 		before	{ visit user_path(user) }
@@ -18,4 +11,34 @@ describe "UserPages" do
 		it	{ should have_title(user.name) }
 		it	{ should have_content(user.name) }
 	end
+
+	describe "signup page" do
+		before	{ visit signup_path }
+
+		it	{ should have_title("Sign Up") }
+		it	{ should have_content("Sign Up") } 
+	end
+
+	describe "signup" do
+		before	{ visit signup_path }
+
+		describe "with invalid information" do
+			it "should not create a user" do
+				expect	{ click_button "Create my account" }.not_to change(User, :count)
+			end
+		end
+
+		# those two "expect" methods check the "create" action which requires a "create" view, so a "create" view should be created to get the test pass ("the title in the view should get created as well")!!
+
+		describe "with valid information" do
+			it "should create a user" do
+				fill_in	"Name",			with: "Jeremy"
+				fill_in "Email",		with: "jwong@yahoo.com"
+				fill_in "Password",		with: "linlin"
+				fill_in "Confirmation",	with: "linlin"
+				expect	{ click_button "Create my account" }.to change(User, :count)
+			end
+		end
+	end
+
 end
